bb425cfaa6c53c655eaef8f3b89bcec6c4d2b184
crl <2080758199@qq.com>
2024-04-08 15:05:16 +0800

    1

--- LA32R-Simulation-Framework/include/dpi_c.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __DPI_C_HPP__
-#define __DPI_C_HPP__
-
-#include <svdpi.h>
-
-extern "C" void expose_memory(int _dataMemDepth, const svLogicVecVal * _srcMem) ;
-extern "C" void expose_configs(int _instMemStart, int _instMemDepth, const char * _instMemFile, int _dataMemStart, int _dataMemDepth, const char * _dataMemFile, char _coreType, unsigned char _io);
-
-#endif // __DPI_C_HPP__
