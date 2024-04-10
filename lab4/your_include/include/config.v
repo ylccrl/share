893e43c4490087c0fe1f73130e1479f8bbb58e79
crl <2080758199@qq.com>
2024-04-10 12:20:36 +0800

    1

--- lab4/your_include/include/config.v
+++ lab4/your_include/include/config.v
@@ -6,20 +6,5 @@
 
 `define HALT_INST           32'H80000000
 
-// logic type
-`define ADD                 5'B00000
-`define SUB                 5'B00010
-`define SLT                 5'B00100
-`define SLTU                5'B00101
-`define NOR                 5'B01000
-`define AND                 5'B01001
-`define OR                  5'B01010
-`define XOR                 5'B01011
-`define SLL                 5'B01110
-`define SRL                 5'B01111
-`define SRA                 5'B10000
-`define SRC0                5'B10001
-`define SRC1                5'B10010
-
 `endif
 
--- lab4/your_vsrc/CPU.v
+++ lab4/your_vsrc/CPU.v
@@ -11,10 +11,10 @@ module CPU (
     input                   [31 : 0]            imem_rdata,
 
 /* ------------------------------ Memory (data) ----------------------------- */
-    input                   [31 : 0]            dmem_rdata, // Unused
-    output                  [ 0 : 0]            dmem_we,    // Unused
-    output                  [31 : 0]            dmem_addr,  // Unused
-    output                  [31 : 0]            dmem_wdata, // Unused
+    input                   [31 : 0]            dmem_rdata, 
+    output                  [ 0 : 0]            dmem_we,    
+    output                  [31 : 0]            dmem_addr,  
+    output                  [31 : 0]            dmem_wdata, 
 
 /* ---------------------------------- Debug --------------------------------- */
     output                  [ 0 : 0]            commit,
@@ -33,7 +33,7 @@ module CPU (
 );
 
 
-// TODO
+
 
 /* -------------------------------------------------------------------------- */
 /*                                    Commit                                  */
@@ -53,6 +53,134 @@ module CPU (
     reg  [31 : 0]   commit_dmem_wa_reg  ;
     reg  [31 : 0]   commit_dmem_wd_reg  ;
 
+
+
+    wire [31:0] cur_pc;
+    PC my_pc (
+        .clk    (clk        ),
+        .rst    (rst        ),
+        .en     (global_en  ),    // 当 global_en 为高电平时，PC 才会更新，CPU 才会执行指令。
+        .npc    (cur_npc    ),
+
+        .pc     (cur_pc     )
+    );
+
+    assign imem_raddr = cur_pc;
+
+    wire [31: 0] inst = imem_rdata;
+
+    wire [ 4: 0] alu_op;
+    wire [ 3: 0] dmem_access;
+    wire [31: 0] imm;
+    wire [ 4: 0] rf_ra_k,rf_ra_j,rf_ra_d;
+    wire [ 0: 0] rf_we,dmem_we;
+    wire [ 1: 0] rf_wd_sel;
+    wire [ 0: 0] alu_src0_sel,alu_src1_sel;
+    wire [ 3: 0] br_type;
+    DECODER my_decoder(
+        .inst(inst),
+        //输出端口
+        .alu_op(alu_op),
+        .dmem_access(dmem_access),
+        .imm(imm),
+        .rf_ra_k(rf_ra_k),
+        .rf_ra_j(rf_ra_j),
+        .rf_ra_d(rf_ra_d),
+        .rf_we(rf_we),
+        .dmem_we(dmem_we),
+        .rf_wd_sel(rf_wd_sel),
+        .alu_src0_sel(alu_src0_sel),
+        .alu_src1_sel(alu_src1_sel),
+        .br_type(br_type)
+    );
+
+    wire [31:0] rf_rd_k,rf_rd_j,rf_rd_d;
+    RF my_rf(
+        .clk       (clk),
+        .rf_ra_k    (rf_ra_k),
+        .rf_ra_j    (rf_ra_j),
+        .rf_ra_d    (rf_ra_d),
+
+        .rf_we     (rf_we),
+        .rf_wd     (rf_wd),
+        //输出端口
+        .rf_rd_k    (rf_rd_k),
+        .rf_rd_j    (rf_rd_j),
+        .rf_rd_d    (rf_rd_d),
+
+        .dbg_reg_ra(debug_reg_ra),
+        .dbg_reg_rd(debug_reg_rd)
+    );
+
+    wire [31:0] alu_src0,alu_src1;
+    MUX mux0(
+        .src0(rf_rd_j),
+        .src1(cur_pc),
+        .sel(alu_src0_sel),
+
+        .res(alu_src0)
+    );
+    MUX mux1(
+        .src0(rf_rd_k),
+        .src1(imm),
+        .sel(alu_src1_sel),
+
+        .res(alu_src1)
+    );
+
+    wire [31:0] alu_res;
+    ALU my_alu(
+        .alu_src0(alu_src0),
+        .alu_src1(alu_src1),
+        .alu_op(alu_op),
+
+        .alu_res(alu_res)
+    );
+    
+    wire [0:0] npc_sel;
+    BRANCH my_branch(
+        .br_type(br_type),
+        .br_src0(rf_rd_j),
+        .br_src1(rf_rd_d),
+
+        .npc_sel(npc_sel)
+    );
+
+    wire [31:0] addr = alu_res;
+    assign dmem_addr = addr;
+    wire [31:0] rd_out;
+    SLU my_slu(
+        .addr(addr),
+        .dmem_access(dmem_access),
+        .rd_in(dmem_rdata),
+        .wd_in(rf_rd_d),
+
+        .rd_out(rd_out),
+        .wd_out(dmem_wdata)
+    );
+
+    wire [31:0] pc_add4 = cur_pc + 32'd4;
+
+    wire [31:0] cur_npc;
+    NPCMUX my_npc(
+        .pc_add4(pc_add4),
+        .pc_offset(alu_res),
+        .npc_sel(npc_sel),
+
+        .npc(cur_npc)
+    );
+
+    wire [31:0] rf_wd;
+    MUX1 my_rf_wd(
+        .src0(pc_add4),
+        .src1(alu_res),
+        .src2(rd_out),
+        .src3(32'b0),
+        .sel(rf_wd_sel),
+
+        .res(rf_wd)
+    );
+    
     always @(posedge clk) begin
         if (rst) begin
             commit_reg          <= 1'H0;
@@ -68,15 +196,15 @@ module CPU (
         end
         else if (global_en) begin
             commit_reg          <= commit_if;
-            commit_pc_reg       <= 0;       // TODO
-            commit_inst_reg     <= 0;       // TODO
-            commit_halt_reg     <= 0;       // TODO
-            commit_reg_we_reg   <= 0;       // TODO
-            commit_reg_wa_reg   <= 0;       // TODO
-            commit_reg_wd_reg   <= 0;       // TODO
-            commit_dmem_we_reg  <= 0;
-            commit_dmem_wa_reg  <= 0;
-            commit_dmem_wd_reg  <= 0;
+            commit_pc_reg       <= cur_pc;       
+            commit_inst_reg     <= inst;       
+            commit_halt_reg     <= (inst == HALT_INST);       
+            commit_reg_we_reg   <= rf_we;       
+            commit_reg_wa_reg   <= rf_ra_d;       
+            commit_reg_wd_reg   <= rf_wd;       
+            commit_dmem_we_reg  <= dmem_we;
+            commit_dmem_wa_reg  <= dmem_addr;
+            commit_dmem_wd_reg  <= dmem_wdata;
         end
     end
 
