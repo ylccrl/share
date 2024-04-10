6d48a057b764efb5eb0dc58654e85e1014a58e43
crl <2080758199@qq.com>
2024-04-10 11:31:13 +0800

    1

--- lab4/your_vsrc/DECODER.v
+++ lab4/your_vsrc/DECODER.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+
 
 `include "inst_type.vh"
 `include "logic_type.vh"
--- lab4/your_vsrc/MUX.v
+++ lab4/your_vsrc/MUX.v
@@ -1,4 +1,4 @@
-`timescale 1ns / 1ps
+
 //////////////////////////////////////////////////////////////////////////////////
 // Company: 
 // Engineer: 
@@ -20,7 +20,13 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module MUX(
+module MUX #(
+    parameter  WIDTH = 32
+    )(
+        input   [WIDTH-1:0] src0,src1,
+        input   [      0:0] sel,
 
+        output  [WIDTH-1:0] res
     );
+    assign res = sel?src1:src0;
 endmodule
